bf83db53e5f1f83eb708a28ac538686774ef2756
Alan Hegewisch <ahegewisch@identitymind.com>
2019-06-28 22:51:32 -0500

    Adds functionality and tests for decimal converter.

--- server/number-converter/src/main/java/com/demo/numberconverter/services/ConverterService.java
+++ server/number-converter/src/main/java/com/demo/numberconverter/services/ConverterService.java
@@ -5,13 +5,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConverterService {
 
+    static final String[] THOUSANDS = {"", "M", "MM", "MMM"};
+    static final String[] HUNDREDS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    static final String[] TENS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static final String[] UNITS = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
     public String convertDecimal(int numValue) {
-        convertNumber(numValue);
-        return "";
+        return convertNumber(numValue);
     }
 
     public String convertNumber(int numValue) {
-        return "";
+        if (numValue < 1 || numValue > 3999) {
+            return("Invalid input");
+        }
+
+        return new StringBuilder()
+                .append(THOUSANDS[numValue / 1000])
+                .append(HUNDREDS[numValue % 1000 / 100])
+                .append(TENS[numValue % 100 / 10])
+                .append(UNITS[numValue % 10])
+                .toString();
     }
 
 }
--- server/number-converter/src/test/java/com/demo/numberconverter/services/ConverterServiceTest.java
+++ server/number-converter/src/test/java/com/demo/numberconverter/services/ConverterServiceTest.java
@@ -1,17 +1,67 @@
 package com.demo.numberconverter.services;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ConverterServiceTest {
 
+    static private final String[] THOUSANDS = {"", "M", "MM", "MMM"};
+    static private final String[] HUNDREDS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    static private final String[] TENS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static private final String[] UNITS = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    @InjectMocks
     ConverterService converterService;
 
     @Test
-    public void convertNumber() {
-        String r = converterService.convertNumber(1);
-        assertEquals(r, "");
+    public void testConvertLessThanFourThousand() {
+        for (int i = 1; i < 4; i++) {
+            String result = converterService.convertNumber((i) * 1000);
+            assertEquals(THOUSANDS[i], result);
+        }
+    }
+
+    @Test
+    public void testConvertLessThanOneThousand() {
+        for (int i = 1; i < 9; i++) {
+            String result = converterService.convertNumber((i) * 100);
+            assertEquals(HUNDREDS[i], result);
+        }
+    }
+
+    @Test
+    public void testConvertLessThanOneHundred() {
+        for (int i = 1; i < 9; i++) {
+            String result = converterService.convertNumber((i) * 10);
+            assertEquals(TENS[i], result);
+        }
+    }
+
+    @Test
+    public void testConvertLessThanTen() {
+        for (int i = 0; i < 9; i++) {
+            String result = converterService.convertNumber(i + 1);
+            assertEquals(UNITS[i], result);
+        }
+    }
+
+    @Test
+    public void testConvertInvalidBounds() {
+        String invalidMessage = "Invalid input";
+
+        String result = converterService.convertNumber(-1);
+        assertEquals(invalidMessage, result);
+
+        result = converterService.convertNumber(0);
+        assertEquals(invalidMessage, result);
+
+        result = converterService.convertNumber(4000);
+        assertEquals(invalidMessage, result);
     }
 
 }
