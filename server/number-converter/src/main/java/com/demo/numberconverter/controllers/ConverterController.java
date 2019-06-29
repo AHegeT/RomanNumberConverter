645fc3f717499ab997ec5d4f5805e87090f25dc7
Alan Hegewisch <ahegewisch@identitymind.com>
2019-06-29 14:27:53 -0500

    Adds basic logging for auditing.

--- server/number-converter/src/main/java/com/demo/numberconverter/controllers/ConverterController.java
+++ server/number-converter/src/main/java/com/demo/numberconverter/controllers/ConverterController.java
@@ -1,28 +1,37 @@
 package com.demo.numberconverter.controllers;
 
 import com.demo.numberconverter.enums.Format;
+import com.demo.numberconverter.services.AuditService;
 import com.demo.numberconverter.services.ConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class ConverterController {
 
     @Autowired
     ConverterService converterService;
+    @Autowired
+    AuditService auditService;
 
-    static final String INVALID_MESSAGE = "Invalid format";
+    static private final String INVALID_MESSAGE = "Invalid format";
 
     @CrossOrigin
     @GetMapping(path = "/convert/{format}")
     public String getRomanConversion(@PathVariable(name = "format") String format,
                                      @RequestParam(value = "value") long numValue) {
 
+        auditService.logConversionRequest(numValue, format);
         if (format == null || format.matches(Format.DECIMAL.getCompleteName())) {
-            return this.converterService.convertDecimalToRoman(numValue);
+            String result = this.converterService.convertDecimalToRoman(numValue);
+            auditService.logConversionResult(numValue, format, result);
+            return result;
 
         } else if (format.matches(Format.BINARY.getCompleteName())) {
-            return this.converterService.convertBinaryToRoman(numValue);
+            String result = this.converterService.convertBinaryToRoman(numValue);
+            auditService.logConversionResult(numValue, format, result);
+            return result;
         }
         return INVALID_MESSAGE;
     }
---
+++ server/number-converter/src/main/java/com/demo/numberconverter/services/AuditService.java
@@ -0,0 +1,27 @@
+package com.demo.numberconverter.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class AuditService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuditService.class);
+
+    public void logConversionRequest(long requestedValue, String requestedFormat) {
+        String message = String
+                .format("%s -- Converting value %d to %s", Instant.now().toString(), requestedValue, requestedFormat);
+        logger.info(message);
+    }
+
+    public void logConversionResult(long requestedValue, String requestedFormat, String requestResult) {
+        String message = String
+                .format("%s -- Converting value %d to %s. Resulted in: %s",
+                        Instant.now().toString(), requestedValue, requestedFormat, requestResult);
+        logger.info(message);
+    }
+
+}
