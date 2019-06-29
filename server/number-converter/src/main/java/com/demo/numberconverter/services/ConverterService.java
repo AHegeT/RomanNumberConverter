a14a590443a9f9b069502ae2062e750ce5e5fe32
Alan Hegewisch <ahegewisch@identitymind.com>
2019-06-28 21:33:13 -0500

    Creates basic Spring structure.

---
+++ server/number-converter/src/main/java/com/demo/numberconverter/services/ConverterService.java
@@ -0,0 +1,4 @@
+package com.demo.numberconverter.services;
+
+public class ConverterService {
+}
---
+++ server/number-converter/src/test/java/com/demo/numberconverter/services/ConverterServiceTest.java
@@ -0,0 +1,7 @@
+package com.demo.numberconverter.services;
+
+import static org.junit.Assert.*;
+
+public class ConverterServiceTest {
+
+}
