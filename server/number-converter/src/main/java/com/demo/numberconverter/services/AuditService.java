260b24b71ef6695b0eef00db91d1efeebb77af88
Alan Hegewisch <ahegewisch@identitymind.com>
2019-06-30 19:28:54 -0500

    Improves audit logging. Makes it condensed and purpose-specific.

--- server/number-converter/src/main/java/com/demo/numberconverter/services/AuditService.java
+++ server/number-converter/src/main/java/com/demo/numberconverter/services/AuditService.java
@@ -4,18 +4,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.time.Instant;
 
 @Service
 public class AuditService {
 
+    private static final String LOG_FILE = "./audit/conversions.log";
     private static final Logger logger = LoggerFactory.getLogger(AuditService.class);
 
     public void logConversionRequest(long requestedValue, String requestedFormat, String requestResult) {
         String message = String
-                .format("%s -- Converting value %d to %s. Resulted in: %s",
+                .format("%s | %d | %s | %s %n",
                         Instant.now().toString(), requestedValue, requestedFormat, requestResult);
-        logger.info(message);
+
+        log(message);
+    }
+
+    private void log(String message) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            writer.write(message);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 
 }
